fc303700f2dfa559131881bf49f636405946787a
alexey <pam.3ebc@gmail.com>
2020-04-05 01:52:47 +0700

    Initial commit

---
+++ src/main/java/ru/plotnikov/telegrambot/endpoint/IndexEndpoint.java
@@ -0,0 +1,14 @@
+package ru.plotnikov.telegrambot.endpoint;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexEndpoint {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+
+}
